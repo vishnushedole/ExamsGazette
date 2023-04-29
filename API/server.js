5f9753aa618187258a18d5057d20428957e988be
vishnu <vishnushedole09@gmail.com>
2023-04-30 04:01:54 +0530

    cors1

--- API/server.js
+++ API/server.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 const Connect = require('./Database/connection.js')
 const global = require('global')
 app.use(cors({
-    origin: ["https://master--examsgazette.netlify.app/"],
+    origin: ["*"],
     methods: ["GET", "POST","FETCH"],
     credentials:true
 }));
@@ -26,7 +26,7 @@ app.use(session({
 app.use(bodyparser.urlencoded({ extended: false }))
 app.use(bodyparser.json())
 app.use((req,res,next)=>{
-    res.setHeader('Access-Control-Allow-Origin','https://master--examsgazette.netlify.app/');
+    res.setHeader('Access-Control-Allow-Origin','*');
     res.setHeader('Access-Control-Allow-Methods','*');
     res.setHeader('Access-Control-Allow-Headers','*');
     next();
