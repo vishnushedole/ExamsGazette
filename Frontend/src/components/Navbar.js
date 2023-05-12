4dfd40e3ad11f239fb2728e33dfc389b7fe18768
vishnu <vishnushedole09@gmail.com>
2023-05-12 19:32:37 +0530

    log message

--- Frontend/src/components/Navbar.js
+++ Frontend/src/components/Navbar.js
@@ -15,7 +15,7 @@ function Navbar() {
   useEffect(() =>{
        axios.defaults.withCredentials = true;
        axios.get('https://examsgazette.onrender.com/isLoggedin').then(res=>{
-      
+        console.log(res)
         if(res.data.user){
           sethref('');
           setBtnText("Sign Out");
