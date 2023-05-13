212a3437b80c9cdc1c264376b38df3726c9e0bf2
Vishnu Shedole <84067052+vishnushedole@users.noreply.github.com>
2023-05-13 20:01:52 +0530

    Update user.js

--- API/Controller/user.js
+++ API/Controller/user.js
@@ -194,6 +194,7 @@ exports.Login = async(req, res,next) =>{
                     
                     req.session.user = req.body.email;
                     req.session.username = result.firstname;
+                    console.log("session id : ");
                     console.log(req.session.id);
                   
                     return res.json({valid: true, user : req.session.user, username:result.firstname});
